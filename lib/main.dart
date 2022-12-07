10e9c436bdfb920f68eb31b9783a8182424d1f1f
Aleksandar Stojanovic <aleksandar.stojanovic@forward.eu>
2022-12-07 23:00:37 +0100

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:wombocombo/providers/boxing_attacks_provider.dart';
-import 'package:wombocombo/screens/choose_martial_art_screen.dart';
-import 'package:wombocombo/screens/combos_screen.dart';
+import 'package:wombocombo/screens/martial_art_attack_mapping/choose_martial_art_screen.dart';
+import 'package:wombocombo/screens/combos/combos_screen.dart';
 import 'package:wombocombo/screens/home_screen.dart';
-import 'package:wombocombo/screens/make_your_combo_screen.dart';
+import 'package:wombocombo/screens/make_your_combo/make_your_combo_screen.dart';
 import 'package:wombocombo/screens/martial_art_attack_mapping/boxing_mapping.dart';
 import 'package:wombocombo/screens/martial_art_attack_mapping/kickboxing_mapping.dart';
 import 'package:wombocombo/screens/martial_art_attack_mapping/muay_thai_mapping.dart';
-import 'package:wombocombo/screens/quick_combos_screen.dart';
+import 'package:wombocombo/screens/martial_art_attack_mapping/quick_combos_screen.dart';
 import 'package:wombocombo/screens/countdown_timer/set_timer_screen.dart';
 import 'package:wombocombo/screens/countdown_timer/timer_new.dart';
 
